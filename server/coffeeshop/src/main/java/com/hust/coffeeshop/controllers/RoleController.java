6f4e6666037cc51fca93423dab2e2ec278dba481
khoavt2 <61816581+thekhoa2206@users.noreply.github.com>
2023-05-26 14:20:28 +0700

    fix: sửa lại code

--- server/coffeeshop/src/main/java/com/hust/coffeeshop/controllers/RoleController.java
+++ server/coffeeshop/src/main/java/com/hust/coffeeshop/controllers/RoleController.java
@@ -4,13 +4,11 @@ import com.hust.coffeeshop.models.dto.role.CreateRoleRequest;
 import com.hust.coffeeshop.models.dto.role.RoleResponse;
 import com.hust.coffeeshop.models.exception.BaseException;
 import com.hust.coffeeshop.services.RoleService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/role")
+@CrossOrigin("http://localhost:3000")
 public class RoleController extends BaseException {
     private final RoleService roleService;
 
--- server/coffeeshop/src/main/java/com/hust/coffeeshop/controllers/UserController.java
+++ server/coffeeshop/src/main/java/com/hust/coffeeshop/controllers/UserController.java
@@ -1,20 +1,15 @@
 package com.hust.coffeeshop.controllers;
 
-import com.hust.coffeeshop.models.dto.customer.CustomerRequest;
-import com.hust.coffeeshop.models.dto.customer.CustomerResponse;
 import com.hust.coffeeshop.models.dto.user.request.CreateUserRequest;
 import com.hust.coffeeshop.models.dto.user.response.UserResponse;
 import com.hust.coffeeshop.models.exception.BaseException;
 import com.hust.coffeeshop.models.exception.ErrorException;
-<<<<<<< .mine
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-=======
 import com.hust.coffeeshop.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
->>>>>>> .theirs
 
 @RestController
 @RequestMapping(value = "/api/user")
@@ -26,11 +21,6 @@ public class UserController extends BaseException {
         this.userService = userService;
     }
 
-    @GetMapping("/test")
-    public String test(){
-        if(1==1) throw new ErrorException("Lỗi rồi!");
-        return "hello";
-    }
     // tạo user mới
     @PostMapping
     public UserResponse create(@RequestBody CreateUserRequest request){
